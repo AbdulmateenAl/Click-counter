106e7f26dceb2097e958b403f32021c4e4cf4db1
Abdulmateen <alabiabdulmateen94@gmail.com>
2023-12-08 21:46:37 +0100

    A simple react app that counts the amount of time you click the button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+export default function Count() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount((prev) => prev + 1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>You clicked me {count} times</h1>
+      <button onClick={handleClick}>click</button>
     </div>
   );
 }
-
-export default App;
